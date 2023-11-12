03702c81a19d94032c153c86d87db158ed6e76e6
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2023-11-12 18:22:19 +0100

    different base dir

--- apps/docs/lib/mdx/generateRefMarkdown.tsx
+++ apps/docs/lib/mdx/generateRefMarkdown.tsx
@@ -18,7 +18,7 @@ async function generateRefMarkdown(sections: ICommonMarkdown[], slug: string) {
     sections.map(async (section) => {
       const baseDir = __dirname.split("/.next")[0];
 
-      const pathName = `${baseDir}/.next/docs/ref${slug}/${section.id}.mdx`;
+      const pathName = `${baseDir}/docs/ref${slug}/${section.id}.mdx`;
 
       function checkFileExists(x) {
         if (fs.existsSync(x)) {
