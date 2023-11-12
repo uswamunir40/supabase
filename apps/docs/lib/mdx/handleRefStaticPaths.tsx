9e941082282c3cf025b0435e960eda70cddf886b
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2023-11-12 17:33:22 +0100

    no static

--- apps/docs/lib/mdx/handleRefStaticPaths.tsx
+++ apps/docs/lib/mdx/handleRefStaticPaths.tsx
@@ -1,6 +1,14 @@
 import { ICommonSection } from '~/components/reference/Reference.types'
 
 async function handleRefGetStaticPaths(sections: ICommonSection[]) {
+  // In preview environments, don't generate static pages (faster builds)
+  if (true) {
+    return {
+      paths: [],
+      fallback: 'blocking',
+    }
+  }
+
   // In production, generate static pages for every sub-section (better SEO)
   return {
     paths: sections.map((section) => {
