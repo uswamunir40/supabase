d0f196d7c7b4d76b83ba86e329b1155d370ab59c
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2023-11-12 19:33:44 +0100

    me

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -32,9 +32,12 @@ const withMDX = nextMdx({
 /** @type {import('next').NextConfig} nextConfig */
 const nextConfig = {
   outputFileTracing: true,
-  outputFileTracingIncludes: {
-    '/docs/reference/**/*': ['./docs/**/*'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/docs/reference/**/*': ['./docs/**/*'],
+    },
   },
+ 
 
   webpack(config, options) {
     /** MDX files in docs folder are loaded dynamically on the server, so they need to be included in the .next build dir */
