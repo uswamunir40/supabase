423873c89994d8fc77b2cb340e914e6c2dac179d
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2023-11-12 16:21:49 +0100

    remove

--- apps/docs/components/reference/RefFunctionSection.tsx
+++ apps/docs/components/reference/RefFunctionSection.tsx
@@ -1,11 +1,8 @@
 import ReactMarkdown from 'react-markdown'
 
-import { CodeBlock, IconDatabase, Tabs } from 'ui'
-
 import RefSubLayout from '~/layouts/ref/RefSubLayout'
 import { extractTsDocNode } from '~/lib/refGenerator/helpers'
 
-import RefDetailCollapse from '~/components/reference/RefDetailCollapse'
 import { IRefFunctionSection } from './Reference.types'
 import components from '~/components'
 
@@ -49,140 +46,6 @@ const RefFunctionSection: React.FC<IRefFunctionSection> = (props) => {
             )}
           </>
         </RefSubLayout.Details>
-        <RefSubLayout.Examples>
-          {item.examples && (
-            <>
-              <div className="overflow-hidden w-full">
-                <Tabs
-                  defaultActiveId={item.examples[0].id}
-                  size="tiny"
-                  type="rounded-pills"
-                  scrollable
-                  queryGroup="example"
-                >
-                  {item.examples &&
-                    item.examples.map((example, exampleIndex) => {
-                      const exampleString =
-                        '' +
-                        (example.code &&
-                          example.code
-                            .trim()
-                            .replace(/^```.*/, '')
-                            .replace(/```$/, ''))
-
-                      const codeBlockLang = example?.code?.startsWith('```js')
-                        ? 'js'
-                        : example?.code?.startsWith('```ts')
-                        ? 'ts'
-                        : example?.code?.startsWith('```dart')
-                        ? 'dart'
-                        : example?.code?.startsWith('```c#')
-                        ? 'csharp'
-                        : example?.code?.startsWith('```kotlin')
-                        ? 'kotlin'
-                        : 'js'
-                      //                     `
-                      // import { createClient } from '@supabase/supabase-js'
-
-                      // // Create a single supabase client for interacting with your database
-                      // const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
-                      // `
-                      const staticExample = item.examples[exampleIndex]
-
-                      const response = staticExample.response
-                      const sql = staticExample?.data?.sql
-                      const tables = staticExample?.data?.tables
-
-                      return (
-                        <Tabs.Panel
-                          id={example.id}
-                          key={example.id}
-                          label={example.name}
-                          className="flex flex-col gap-3"
-                        >
-                          <CodeBlock
-                            className="useless-code-block-class"
-                            language={codeBlockLang}
-                            hideLineNumbers={true}
-                          >
-                            {exampleString}
-                          </CodeBlock>
-
-                          {((tables && tables.length > 0) || sql) && (
-                            <RefDetailCollapse
-                              id={`${example.id}-${exampleIndex}-data`}
-                              label="Data source"
-                              defaultOpen={false}
-                            >
-                              <>
-                                {tables &&
-                                  tables.length > 0 &&
-                                  tables.map((table) => {
-                                    return (
-                                      <div className="bg-surface-100 border rounded prose max-w-none">
-                                        <div className="bg-background px-5 py-2">
-                                          <div className="flex gap-2 items-center">
-                                            <div className="text-brand">
-                                              <IconDatabase size={16} />
-                                            </div>
-                                            <h5 className="text-xs text-foreground">
-                                              {table.name}
-                                            </h5>
-                                          </div>
-                                        </div>
-                                      </div>
-                                    )
-                                  })}
-                                {sql && (
-                                  <CodeBlock
-                                    className="useless-code-block-class my-0 border border-t-0 border-default !rounded-tl-none !rounded-tr-none"
-                                    language="sql"
-                                    hideLineNumbers={true}
-                                  >
-                                    {sql.replace(/sql/g, '').replace(/```/g, '')}
-                                  </CodeBlock>
-                                )}
-                              </>
-                            </RefDetailCollapse>
-                          )}
-
-                          {response && (
-                            <RefDetailCollapse
-                              id={`${example.id}-${exampleIndex}-response`}
-                              label="Response"
-                              defaultOpen={false}
-                            >
-                              <CodeBlock
-                                className="useless-code-block-class rounded !rounded-tl-none !rounded-tr-none border border-default"
-                                language={codeBlockLang}
-                                hideLineNumbers={true}
-                              >
-                                {response.replace(/```/g, '').replace('json', '')}
-                              </CodeBlock>
-                            </RefDetailCollapse>
-                          )}
-
-                          {example.description && (
-                            <RefDetailCollapse
-                              id={`${example.id}-${exampleIndex}-notes`}
-                              label="Notes"
-                              defaultOpen={false}
-                            >
-                              <div className="bg-surface-100 border border-default rounded !rounded-tl-none !rounded-tr-none prose max-w-none px-5 py-2">
-                                <ReactMarkdown className="text-sm">
-                                  {example.description}
-                                </ReactMarkdown>
-                              </div>
-                            </RefDetailCollapse>
-                          )}
-                        </Tabs.Panel>
-                      )
-                    })}
-                </Tabs>
-              </div>
-            </>
-          )}
-        </RefSubLayout.Examples>
       </RefSubLayout.Section>
     </>
   )
