cee8d181d097929a0f03d13725cca56b35292760
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2023-11-12 17:03:04 +0100

    ref

--- apps/docs/lib/docs.ts
+++ apps/docs/lib/docs.ts
@@ -1,93 +1,99 @@
-import fs from 'fs'
-import { join } from 'path'
-import matter from 'gray-matter'
-import nonGeneratedReferencePages from 'data/nonGeneratedReferencePages'
-import { REFERENCES } from '~/components/Navigation/NavigationMenu/NavigationMenu.constants'
+import fs from "fs";
+import { join } from "path";
+import matter from "gray-matter";
+import nonGeneratedReferencePages from "data/nonGeneratedReferencePages";
+import { REFERENCES } from "~/components/Navigation/NavigationMenu/NavigationMenu.constants";
 
-const docsDirectory = process.cwd()
+const docsDirectory = process.cwd();
 
 const getPathToGeneratedDoc = (slug: string) => {
-  const sections = slug.split('/')
-  const updatedSections = sections.slice()
-  updatedSections.splice(updatedSections.length - 1, 0, 'generated')
-  return updatedSections.join('/')
-}
+  const sections = slug.split("/");
+  const updatedSections = sections.slice();
+  updatedSections.splice(updatedSections.length - 1, 0, "generated");
+  return updatedSections.join("/");
+};
 
 export function getDocsBySlug(slug: string) {
-  const realSlug = slug.replace(/\.mdx$/, '')
+  const realSlug = slug.replace(/\.mdx$/, "");
 
   const formattedSlug =
-    (realSlug.includes('reference/javascript/') &&
+    (realSlug.includes("reference/javascript/") &&
       !nonGeneratedReferencePages.includes(realSlug)) ||
-    (realSlug.includes('reference/cli/') && !nonGeneratedReferencePages.includes(realSlug))
+    (realSlug.includes("reference/cli/") &&
+      !nonGeneratedReferencePages.includes(realSlug))
       ? getPathToGeneratedDoc(realSlug)
-      : realSlug
+      : realSlug;
 
   // files can either be .md or .mdx
   // we need to check which one exists
-  let fullPath
-  let fullPathMD = join(docsDirectory, `${formattedSlug}.md`)
-  let fullPathMDX = join(docsDirectory, `${formattedSlug}.mdx`)
+  let fullPath;
+  let fullPathMD = join(docsDirectory, `${formattedSlug}.md`);
+  let fullPathMDX = join(docsDirectory, `${formattedSlug}.mdx`);
+
+  console.log({ fullPathMDX, fullPathMD });
 
   if (fs.existsSync(fullPathMD)) {
-    fullPath = fullPathMD
+    fullPath = fullPathMD;
   }
 
   if (fs.existsSync(fullPathMDX)) {
-    fullPath = fullPathMDX
+    fullPath = fullPathMDX;
   }
 
   // if no match, 404
   if (!fs.existsSync(fullPath)) {
-    console.log(`\nfile ${fullPath} not found, redirect to 404\n`)
-    fullPath = join(docsDirectory, 'pages/404.mdx')
+    console.log(`\nfile ${fullPath} not found, redirect to 404\n`);
+    fullPath = join(docsDirectory, "pages/404.mdx");
   }
 
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const fileContents = fs.readFileSync(fullPath, "utf8");
 
-  const { data, content } = matter(fileContents)
+  const { data, content } = matter(fileContents);
 
   // Append the title as the h1 tag in the content
-  const formattedContent = data.title !== undefined ? `# ${data.title} \n\n${content}` : content
+  const formattedContent =
+    data.title !== undefined ? `# ${data.title} \n\n${content}` : content;
 
-  return { slug: realSlug, meta: data, content: formattedContent }
+  return { slug: realSlug, meta: data, content: formattedContent };
 }
 
 export function getAllDocs() {
-  const slugs = walk('docs')
-  const docs = slugs.map((slug) => getDocsBySlug(slug))
+  const slugs = walk("docs");
+  const docs = slugs.map((slug) => getDocsBySlug(slug));
 
-  return docs
+  return docs;
 }
 
 function walk(dir: string) {
-  let results: string[] = []
-  const list = fs.readdirSync(dir)
+  let results: string[] = [];
+  const list = fs.readdirSync(dir);
   list.forEach(function (file) {
-    file = dir + '/' + file
-    let slugs = []
+    file = dir + "/" + file;
+    let slugs = [];
 
     fs.readdirSync(dir).forEach((file) => {
-      let absolute = join(dir, file)
+      let absolute = join(dir, file);
       if (fs.statSync(absolute).isDirectory()) {
-        fs.readdirSync(absolute).forEach((subFile) => slugs.push(file.concat('/' + subFile)))
+        fs.readdirSync(absolute).forEach((subFile) =>
+          slugs.push(file.concat("/" + subFile))
+        );
       }
-    })
-  })
-  return results
+    });
+  });
+  return results;
 }
 
 // [Joshen] Initially tried to simplify the NavBar versioning by reading the directory
 // but caused some issues on Vercel. Just gonna park this for now.
 export function getLibraryVersions(slug: string) {
-  const paths = slug.split('/')
-  if (paths.includes('reference') && paths.length >= 3) {
-    const reference = REFERENCES[paths[2]]
+  const paths = slug.split("/");
+  if (paths.includes("reference") && paths.length >= 3) {
+    const reference = REFERENCES[paths[2]];
     if (reference?.library !== undefined) {
-      const path = `data/nav/${reference.library}`
-      const list = fs.readdirSync(path).map((file) => file.split('.')[0])
-      return list.reverse()
+      const path = `data/nav/${reference.library}`;
+      const list = fs.readdirSync(path).map((file) => file.split(".")[0]);
+      return list.reverse();
     }
   }
-  return []
+  return [];
 }
--- apps/docs/lib/mdx/generateRefMarkdown.tsx
+++ apps/docs/lib/mdx/generateRefMarkdown.tsx
@@ -16,7 +16,7 @@ async function generateRefMarkdown(sections: ICommonMarkdown[], slug: string) {
    */
   await Promise.all(
     sections.map(async (section) => {
-      const pathName = `docs/ref${slug}/${section.id}.mdx`;
+      const pathName = `./docs/ref${slug}/${section.id}.mdx`;
 
       console.info({ pathName });
 
